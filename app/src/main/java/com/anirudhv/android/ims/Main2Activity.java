68d2aba1b31d0e2143cedf24f183553269120738
anirudh005 <anirudh.v.1992@gmail.com>
2017-02-11 15:38:13 +0530

    V2

--- app/src/main/java/com/anirudhv/android/ims/Main2Activity.java
+++ app/src/main/java/com/anirudhv/android/ims/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.anirudhv.android.ims;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Main2Activity extends AppCompatActivity  {
 
@@ -16,6 +23,7 @@ public class Main2Activity extends AppCompatActivity  {
     private TextView signup;
     private Button login;
     private Button back;
+    private FirebaseAuth FBA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,30 @@ public class Main2Activity extends AppCompatActivity  {
             public void onClick(View v) {
                 Intent intent = new Intent(Main2Activity.this, MainActivity.class);
                 Main2Activity.this.startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FBA = FirebaseAuth.getInstance();
+                FBA.signInWithEmailAndPassword(username.getText().toString().trim(), password.getText().toString().trim())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(Main2Activity.this,"Successful",Toast.LENGTH_SHORT).show();
+                                    finish();
+                                    Intent intent = new Intent(Main2Activity.this,Main3Activity.class);
+                                    Main2Activity.this.startActivity(intent);
+                                }
+                                else
+                                {
+                                    Toast.makeText(Main2Activity.this,"Fail",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
